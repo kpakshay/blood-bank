42cf81c760e2ab862b9203c2197017919c8ebd7a
Akshay K P <71068656+kpakshay@users.noreply.github.com>
2022-01-07 18:21:12 +0530

    Delete connection.js

--- node-blood-bank/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-exports.connect=()=>{
-    try{
-        mongoose.connect("mongodb://localhost:27017/bloodbank");
-    }catch(err){
-        console.log(err)
-    }
-    
-}
